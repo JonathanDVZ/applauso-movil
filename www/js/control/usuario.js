cc6d58a18600385f6092ce33ad4a3f2a30828633
blas2508 <blasparra2508@gmail.com>
2017-09-27 13:46:02 -0430

    aggTarjetas

--- www/js/control/usuario.js
+++ www/js/control/usuario.js
@@ -43,3 +43,83 @@ function cambiarPass(data){
 function cambiarImg(data){
 	console.log(data);	
 }
+
+function aggTarjeta(datos){
+	modal_wait('Procesando...',1,1);
+	$.ajax({
+		url:SERVERNODE+'/usuario/agregar_tarjeta',
+		type:'POST',
+		data:datos+'&cod_u='+getCookie('id_usuario'),
+		crossDomain:true,
+		cache:false,
+		success:function(d){
+			modal_wait('',0,0);
+			console.log(d);
+			if(d.r==true){
+				modal_alert('Tarjeta Agregada',1);
+				$('#formTar')[0].reset();
+			}else{
+				modal_alert(d.msj,1);
+			}
+		}
+	});
+}
+
+function obtenerTarjetas(){
+	$.ajax({
+		url:SERVERNODE+'/usuario/obtener-tarjetas?cod_usu='+getCookie('id_usuario')+'',
+		type:'GET',
+		crossDomain:true,
+		cache:false,
+		success:function(d){
+			console.log(d);
+			var html = '';
+			if(d.r==true){
+				for(var i = d.data.length-1; i >= 0; i--){
+					var nro = d.data[i].nume_tar;
+					 var n = nro.split(' ');
+					html+='<li class="list-group-item">';
+						html+='<a href="aggTarjeta.html?cod_tar='+d.data[i].code_tar+'" style="color:#333;">';
+							html+='<img src="img/Recursos-63.png">';
+							html+='<span class="fa fa-asterisk"></span>';
+							html+='<span class="fa fa-asterisk"></span>';
+							html+='<span class="fa fa-asterisk"></span>';
+							html+='<span class="fa fa-asterisk"></span>';
+							html+=' <span>'+n[3]+'</span>';
+						html+='</a>';
+						html+=' <a onclick="eleTarjeta('+d.data[i].code_tar+')"><span style="color:#ce2a2a;" class="fa fa-trash fa-lg"></span></a>';
+					html+='</li>';
+				}
+				$('.lista_tarjetas').append(html);
+			}else{
+				html+='<li class="list-group-item">';
+					html+='<b>Aun no Agregas una Tarjeta</b>';
+				html+='</li>';
+				$('.lista_tarjetas').append(html);
+			}
+		}
+	});
+}
+
+function eleTarjeta(cod_tar){
+	var msj = confirm('¿Seguro Que Desea Eliminar?');
+	if(msj==true){
+		modal_wait('Procesando...',1,1);
+		$.ajax({
+			url:SERVERNODE+'/usuario/eliminar-tarjeta?cod_tar='+cod_tar,
+			type:'POST',
+			crossDomain:true,
+			cache:false,
+			success:function(d){
+				modal_wait('',0,0);
+				console.log(d);
+				if(d.r==true){
+					modal_alert(d.msj,1);
+					setTimeout(function(){location.href = 'pagos.html';},2000);
+				}else{
+					modal_alert('No se Elimino',1);
+				}
+			}
+		});
+	}
+}
